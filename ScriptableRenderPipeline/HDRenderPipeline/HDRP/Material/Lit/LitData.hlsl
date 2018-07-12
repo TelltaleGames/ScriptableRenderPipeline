5e5ff84cc14d12bee890b2e3dcdd047edcd2a864
Jayeson Lee-Steere <jayeson.lee-steere@stirlinglabs.com>
2018-07-12 20:04:41 +0800

    Change asperity clamp to use saturate.

--- ScriptableRenderPipeline/HDRenderPipeline/HDRP/Material/Lit/LitData.hlsl
+++ ScriptableRenderPipeline/HDRenderPipeline/HDRP/Material/Lit/LitData.hlsl
@@ -231,7 +231,7 @@ void GetSurfaceAndBuiltinData(FragInputs input, float3 V, inout PositionInputs p
     }
 #endif
 
-    float asperityMix = _AsperityAmount * pow( 1.0 - clamp(dot(surfaceData.normalWS, V), 0, 1), _AsperityExponent );
+    float asperityMix = _AsperityAmount * pow( 1.0 - saturate(dot(surfaceData.normalWS, V)), _AsperityExponent );
     surfaceData.baseColor.rgb = lerp(surfaceData.baseColor.rgb, float3(1.0,1.0,1.0), asperityMix);
 
     // Caution: surfaceData must be fully initialize before calling GetBuiltinData
