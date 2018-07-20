f1cf7083f4021cc362326aa7d061f9a207c7d413
Jayeson Lee-Steere <jayeson.lee-steere@stirlinglabs.com>
2018-07-20 18:48:40 +0800

    Workaround for compile error in DecalData.hlsl (still needs to have changes integrated into it).

--- com.unity.render-pipelines.high-definition/HDRP/Material/Decal/DecalData.hlsl
+++ com.unity.render-pipelines.high-definition/HDRP/Material/Decal/DecalData.hlsl
@@ -16,6 +16,8 @@ void GetSurfaceData(FragInputs input, out DecalSurfaceData surfaceData)
     surfaceData.normalWS = float4(0,0,0,0);
     surfaceData.mask = float4(0,0,0,0);
     surfaceData.HTileMask = 0;
+    surfaceData.alpha = 1; // TODO: @Brandon, you'll need to reapply the your changes to this file - Jayeson.
+    
 #if (SHADERPASS == SHADERPASS_DBUFFER_PROJECTOR)
     float totalBlend = clamp(normalToWorld[0][3], 0.0f, 1.0f);
     float2 scale = float2(normalToWorld[3][0], normalToWorld[3][1]);
@@ -25,6 +27,7 @@ void GetSurfaceData(FragInputs input, out DecalSurfaceData surfaceData)
 	float totalBlend = _DecalBlend;
 	float2 texCoords = input.texCoord0;
 #endif
+
 
 #if _COLORMAP
     surfaceData.baseColor *= SAMPLE_TEXTURE2D(_BaseColorMap, sampler_BaseColorMap, texCoords);    
